df0229bc0e163c69abe99e6f37158616e28eec44
abhilashaPabbi <56274579+abhilashaPabbi@users.noreply.github.com>
2019-10-12 11:22:07 -0700

    Add files via upload

--- MergeIntervals.hs
+++ MergeIntervals.hs
@@ -1,12 +1,9 @@
 import Data.List
 -- Given the input (a list of tuples), check if there is any overlap, if so merge the tuples into one ---
 
--- create a new type interval which is a tuple of integers
-type Interval = (Integer, Integer)
-
--- Function that is comparing two intervals and checking if they overlap, returns True if intervals overlap, False otherwise 
--- assumption, that our interval are ordered so the interval that occurs first (in 24 hour clock) will always be the first interval input
-checkOverlap :: Interval -> Interval -> Bool
+-- Function that is comparing two tuples and checking if they overlap, returns True if tuples overlap, False otherwise 
+-- assumption, that our tuples are ordered so the tuple that comes first (in 24 hour clock) will always be the function's first input 
+checkOverlap :: (Int, Int) -> (Int, Int) -> Bool
 checkOverlap (0,0) _ = False 
 checkOverlap _ (0,0) = False 
 checkOverlap (a,b) (c,d) = 
@@ -19,9 +16,9 @@ checkOverlap (a,b) (c,d) =
 -- checkOverlap (1,6) (2,5); Expecting True; Result is True 
 -- checkOverlap (2,5) (2,5); Expecting True; Result is True 
 
--- Function that combines/merges two intervals into one, returns the correct interval with min of the two start times 
+-- Function that combines/merges two tuples into one, returns the correct tuple with min of the two start times 
 -- and max of the two end times 
-merge2Intervals :: Interval -> Interval -> Interval
+merge2Intervals :: (Int, Int) -> (Int, Int) -> (Int, Int)
 merge2Intervals (a,b) (c,d) = (min a c, max b d)
 
 -- TESTS:
@@ -31,9 +28,9 @@ merge2Intervals (a,b) (c,d) = (min a c, max b d)
 -- merge (1,6) (2,5); Expecting (1,6); Result is (1,6) 
 -- merge (2,5) (2,5); Expecting (2,5); Result is (2,5) 
 
--- Function that sorts the given list of intervals only by first element of tuple
+-- Function that sorts the given list of tuples only by first element of tuple
 -- basically checking the first value in the tuple and comparing it to the next 
-sortIntervals:: [Interval] -> [Interval]
+sortIntervals:: [(Int, Int)] -> [(Int, Int)]
 sortIntervals = sortBy (\ (a,_) (c,_) -> (compare a c))
 
 -- TESTS:
@@ -41,9 +38,9 @@ sortIntervals = sortBy (\ (a,_) (c,_) -> (compare a c))
 -- sortIntervals [(3,6), (1,3), (1,2)]; Expecting [(1,3), (1,2), (3,6)]; Result is [(1,3), (1,2), (3,6)]
 -- sortIntervals [(1,2), (3,4), (5,16)]; Expecting [(1,2), (3,4), (5,16)]; Result is [(1,2), (3,4), (5,16)]
 
--- Function that is going through sorted list of intervals, merging any intervals that are overlapping 
--- and then appending it to a list to get a final sorted and merged interval list 
-checkAndMergeIntervals :: [(Interval)] -> [(Interval)]
+-- Function that is going through sorted list of tuples, merging any tuples that are overlapping 
+-- and then appending it to a list to get a final sorted and merged tuple list 
+checkAndMergeIntervals :: [(Int, Int)] -> [(Int, Int)]
 checkAndMergeIntervals [] = []
 checkAndMergeIntervals interval = 
     foldr mapThroughList [] (sortIntervals interval)
@@ -53,6 +50,19 @@ checkAndMergeIntervals interval =
 
 -- TESTS:
 -- checkAndMergeIntervals [(3,6), (1,3), (9,16)]; Expecting [(1,6), (9,16)]; Result is [(1,6), (9,16)]
-
+-- checkAndMergeIntervals [(3,6), (1,3), (5,16)]; Expecting [(1,16)]; Result is [(1,16)]
+-- checkAndMergeIntervals [(6,10), (7,9), (4,5), (1,4), (7,9)]; Expecting [(1,5), (6,10)]; Result is [(1,5), (6,10)]
     
---TODO: TEST BASE CASES and check for exceptional cases 
+-- Function that takes the original list of tuples, applies the checkAndMergeIntervals function to get the final sorted and merged tuple list
+-- and then writes to output file a message that outlines thier orginal input vs final output 
+writeOuputToFile :: [(Int, Int)] -> IO ()
+writeOuputToFile input = 
+    writeFile "mergeOutput.txt" outputText
+ where 
+    inputText = input
+    funcOutput = checkAndMergeIntervals input 
+    outputText = "Hello! Here are your results from using Merge Intervals: \n" ++ "input: " ++ (show inputText) ++ "\n" 
+     ++ "output: " ++ (show funcOutput) ++ "\n" ++ "Thanks for using Scheduler! \n" 
+     
+-- writeOuputToFile [(3,6), (1,3), (9,16)]; Expecting correct message with output = [(1,6), (9,16)]; Result is correct message within file
+-- writeOuputToFile [(7,9), (3,5), (7,9)]; Expecting correct message with output = [(3,5),(7,9)]; Result is correct message within file 
